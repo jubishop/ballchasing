e0c4d817d56de5826c3e6f2018acd8e679cbc6e3
Justin Bishop <jubishop@gmail.com>
2020-05-27 23:05:31 -0700

    stop catsing to integer user ids

--- lib/objects/player_id.rb
+++ lib/objects/player_id.rb
@@ -3,9 +3,6 @@ module Ballchasing
   PlayerID = KVStruct.new(%i[id player_number platform]) {
     def initialize(args)
       args.transform_keys!(&:to_sym)
-      if args[:id] && Integer(args[:id], exception:false)
-        args[:id] = Integer(args[:id], 10)
-      end
       super(args)
     end
   }
