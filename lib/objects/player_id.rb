c3499c02cbb63fc6991f90b4fb3391f5e5581efe
Justin Bishop <jubishop@gmail.com>
2020-05-27 21:29:29 -0700

    coercing integers to being integers

--- lib/objects/player_id.rb
+++ lib/objects/player_id.rb
@@ -1,5 +1,13 @@
 require 'rstruct'
 module Ballchasing
-  PlayerID = KVStruct.new(%i[id player_number platform])
+  PlayerID = KVStruct.new(%i[id player_number platform]) {
+    def initialize(args)
+      args.transform_keys!(&:to_sym)
+      if args[:id] && Integer(args[:id], exception:false)
+        args[:id] = Integer(args[:id], 10)
+      end
+      super(args)
+    end
+  }
   private_constant :PlayerID
 end
--- lib/objects/uploader.rb
+++ lib/objects/uploader.rb
@@ -4,6 +4,7 @@ module Ballchasing
   Uploader = KVStruct.new(:steam_id, :name, :profile_url, :avatar) {
     def initialize(args)
       args.transform_keys!(&:to_sym)
+      args[:steam_id] = Integer(args.fetch(:steam_id), 10)
       args[:profile_url] = URI(args.fetch(:profile_url))
       args[:avatar] = URI(args.fetch(:avatar))
       super(args)
