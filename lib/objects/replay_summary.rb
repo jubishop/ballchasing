1b00d0aea066a7fbbeffd58de19401dbf1edeae1
Justin Bishop <jubishop@gmail.com>
2020-06-16 18:44:02 -0700

    update initializers

--- lib/objects/replay_summary.rb
+++ lib/objects/replay_summary.rb
@@ -37,9 +37,9 @@ module Ballchasing
       args[:created] = DateTime.rfc3339(args.fetch(:created))
       args[:date] = DateTime.rfc3339(args.fetch(:date))
       args[:link] = URI(args.fetch(:link))
-      args[:blue] = TeamSummary.new(args.fetch(:blue))
-      args[:orange] = TeamSummary.new(args.fetch(:orange))
-      args[:uploader] = Uploader.new(args.fetch(:uploader))
+      args[:blue] = TeamSummary.new(**args.fetch(:blue))
+      args[:orange] = TeamSummary.new(**args.fetch(:orange))
+      args[:uploader] = Uploader.new(**args.fetch(:uploader))
       args[:max_rank] = Rank.new(args.fetch(:max_rank)) if args[:max_rank]
       args[:min_rank] = Rank.new(args.fetch(:min_rank)) if args[:min_rank]
       args[:groups]&.map! { |group| ReplayGroup.new(group) }
