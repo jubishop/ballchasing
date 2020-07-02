4efeee4bdf79ea7bb50a2af7adb10782cf00b7d3
Justin Bishop <jubishop@gmail.com>
2020-07-02 11:22:12 -0700

    treat duration as duration

--- lib/structs/bc_struct.rb
+++ lib/structs/bc_struct.rb
@@ -4,7 +4,9 @@ module Ballchasing
   class BCStruct < KVStruct
     def initialize(args)
       args.each { |key, value|
-        args[key] = value.seconds if key.start_with?('time')
+        if key.start_with?('time') || key.end_with?('duration')
+          args[key] = value.seconds
+        end
       }
       super(args)
     end
